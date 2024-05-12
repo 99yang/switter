72923763c0a512b2e8b3dd2bb70270411cff7284
99yang <zpo133@naver.com>
2024-05-12 17:21:09 +0900

    #2.3 firebase

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDxd_Ue76OJi5rSPtx1el1dB7zRMAZqCAk',
+  authDomain: 'switter-react.firebaseapp.com',
+  projectId: 'switter-react',
+  storageBucket: 'switter-react.appspot.com',
+  messagingSenderId: '97334048712',
+  appId: '1:97334048712:web:ed07caabb21057230f32fd',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
