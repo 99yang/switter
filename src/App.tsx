8fb5c66861b0247348290f2b1d39cabcfc7df43d
99yang <zpo133@naver.com>
2024-05-19 17:37:50 +0900

    #3.3 firebase: auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from './routes/home';
 import Profile from './routes/profile';
 import CreateAccount from './routes/create-account';
 import Login from './routes/login';
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import { useEffect, useState } from 'react';
 import LoadingScreen from './components/loading-screen';
@@ -48,6 +48,11 @@ const GlobalStyles = createGlobalStyle`
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
  }
 `;
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   const [isLoading, setLoading] = useState(true);
@@ -61,10 +66,10 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
       {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
-    </>
+    </Wrapper>
   );
 }
 
