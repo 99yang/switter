cdc1efc7e0fbfcae83b8364bc96bd1075e7f0c27
99yang <zpo133@naver.com>
2024-08-27 08:08:29 +0900

    #4.3: uploading images

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, updateDoc } from 'firebase/firestore';
 import { useState } from 'react';
 import styled from 'styled-components';
-import { auth, db } from '../firebase';
+import { auth, db, storage } from '../firebase';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 const Form = styled.form`
   display: flex;
@@ -62,15 +63,20 @@ const SubmitBtn = styled.input`
 `;
 
 export default function PostTweetForm() {
+  const FILE_SIZE_MAX_LIMIT = 1 * 1024 * 1024;
   const [isLoading, setLoading] = useState(false);
-  const [tweet, SetTweet] = useState('');
-  const [file, SetFile] = useState<File | null>(null);
+  const [tweet, setTweet] = useState('');
+  const [file, setFile] = useState<File | null>(null);
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    SetTweet(e.target.value);
+    setTweet(e.target.value);
   };
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
     if (files && files.length === 1) {
+      if (files[0].size > FILE_SIZE_MAX_LIMIT) {
+        alert('The maximum capacity that can be uploaded is 1mb');
+        return;
+      }
       setFile(files[0]);
     }
   };
@@ -82,12 +88,25 @@ export default function PostTweetForm() {
 
     try {
       setLoading(true);
-      await addDoc(collection(db, 'tweets'), {
+      const doc = await addDoc(collection(db, 'tweets'), {
         tweet,
         createdAt: Date.now(),
         username: user.displayName || 'unknown',
         userId: user.uid,
       });
+      if (file) {
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc, {
+          photo: url,
+        });
+      }
+      setTweet('');
+      setFile(null);
     } catch (e) {
       console.log(e);
     } finally {
@@ -98,6 +117,7 @@ export default function PostTweetForm() {
   return (
     <Form onSubmit={onSubmit}>
       <TextArea
+        required
         rows={5}
         maxLength={180}
         onChange={onChange}
